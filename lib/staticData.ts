8e38a13784ed21b4934ae681a43090faa73c0e09
Mobina <mobina.ysf203@gmail.com>
2024-10-16 01:17:10 +0330

    chore (staticData):

---
+++ lib/staticData.ts
@@ -0,0 +1,66 @@
+interface menuItem {title: string, url: string, subMenu : menuItem[], icon: string}
+
+export const menu: menuItem[] = [
+    {title: "Home", url: "/", icon:"", subMenu:[]},
+    {title: "Shop", url:"", icon:"", subMenu :
+            [
+                {title: "electronics", url: "/shop/electronics", icon: "/svgs/devices_24dp.svg", subMenu:[]},
+                {title: "jewelery", url: "/shop/jewelery", icon: "/svgs/diamond_24dp.svg", subMenu:[]},
+                {title: "clothing", url: "/shop/clothing", icon: "/svgs/checkroom_24dp.svg", subMenu:[]}
+            ]
+    },
+    {title: "Contact Us", url: "/contact", icon:"", subMenu:[]},
+    {title: "Blog", url: "/blog", icon:"", subMenu:[]}
+];
+
+export interface footerLinks {
+    title: string;
+    url: string;
+}
+
+export const footerLinks1: footerLinks[] = [
+    {
+        title: "My Account",
+        url: "/"
+    },
+    {
+        title: "Login",
+        url: "/auth/login"
+    },
+    {
+        title: "My Cart",
+        url: "/cart"
+    },
+    {
+        title: "My Wishlist",
+        url: "/"
+    },
+    {
+        title: "Checkout",
+        url: "/cart"
+    }
+];
+
+
+export const footerLinks2: footerLinks[] = [
+    {
+        title: "About Us",
+        url: "/contact"
+    },
+    {
+        title: "Careers",
+        url: "/"
+    },
+    {
+        title: "Delivery Information",
+        url: "/cart"
+    },
+    {
+        title: "Privacy Policy",
+        url: "/"
+    },
+    {
+        title: "Terms and Conditions",
+        url: "/"
+    }
+];
